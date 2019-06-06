a2e6af5fdfd1a712fb893791e4991140e267126d
Greg Fruin <gfruin@gmail.com>
2019-06-06 13:55:48 -0500

    created login and logout functions; passed down to Login,Logout,Register

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
+class App extends Component {
   constructor() {
     super();
     this.state = {
@@ -11,15 +11,31 @@ class App extends Component{
       email: '',
       password: '',
     }
+
+    login = (email) => {
+      this.setState({
+        logged: true,
+        email: email
+      })
+    }
+    logout = (email) => {
+      this.setState({
+        logged: false,
+        email: email
+      })
+    }
+
+
     render() {
       return (
-        
-
-
-
-
+        <div className="App">
+          <Login login={this.login}/>
+          <Logout logout={this.logout}/>
+          <Register login={this.login}/>
+        </div>
         )
     }
   }
+}
 
 export default App;
